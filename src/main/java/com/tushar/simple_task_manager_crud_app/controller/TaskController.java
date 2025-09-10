5bc4c8e01a7e05598c8357d89e4bf6062f6a2b55
TusharOnPoint <tushar23das@gmail.com>
2025-09-11 02:29:45 +0600

    filtering and searching added

--- src/main/java/com/tushar/simple_task_manager_crud_app/controller/TaskController.java
+++ src/main/java/com/tushar/simple_task_manager_crud_app/controller/TaskController.java
@@ -21,9 +21,25 @@ public class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
     @PostMapping("/new")
-    public ResponseEntity<Task> createTask(@RequestBody Task task) {
-        logger.info("Creating new task: {}", task);
-        return ResponseEntity.ok(taskService.createTask(task));
+    public ResponseEntity<?> createTask(@RequestBody Task task) {
+        try {
+            if (task.getTitle() == null || task.getTitle().isBlank()) {
+                return ResponseEntity
+                        .badRequest()
+                        .body("Task title must not be empty.");
+            }
+
+            Task createdTask = taskService.createTask(task);
+            return ResponseEntity
+                    .status(201)
+                    .body(createdTask);
+
+        } catch (Exception e) {
+            logger.error("Error while creating task: {}", e.getMessage());
+            return ResponseEntity
+                    .status(500)
+                    .body("An unexpected error occurred.");
+        }
     }
 
     @GetMapping("/all")
@@ -31,22 +47,22 @@ public class TaskController {
         logger.info("Fetching all tasks");
         return ResponseEntity.ok(taskService.getAllTask());
     }
-    
+
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
         logger.info("Fetching task with ID: {}", id);
         Task task = taskService.getTask(id);
-        if(task!=null)
+        if (task != null)
             return ResponseEntity.ok(task);
         else
             return ResponseEntity.notFound().build();
     }
-    
+
     @PutMapping("/{id}")
     public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task) {
         logger.info("Updating task with ID: {}", id);
         Task upTask = taskService.updateTask(id, task);
-        if(upTask!=null)
+        if (upTask != null)
             return ResponseEntity.ok(upTask);
         else
             return ResponseEntity.notFound().build();
@@ -63,5 +79,15 @@ public class TaskController {
             logger.warn("Task with ID: {} not found for deletion", id);
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<List<Task>> getTasksByStatus(@RequestParam String status) {
+        return ResponseEntity.ok(taskService.getTasksByStatus(status));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Task>> searchTasks(@RequestParam String keyword) {
+        return ResponseEntity.ok(taskService.searchTasks(keyword));
     }
 }
